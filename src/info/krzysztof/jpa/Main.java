a21ac06479724de44d2677ca9d2daad31eb556ad
KrzysztofSikora <listdlakrzycha@gmail.com>
2016-03-27 18:37:25 +0200

    Employee

--- src/info/krzysztof/jpa/Main.java
+++ src/info/krzysztof/jpa/Main.java
@@ -1,5 +1,7 @@
 package info.krzysztof.jpa;
 
+import info.krzysztof.jpa.domain.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -11,6 +13,26 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        System.out.println("Działa");
+
+        Employee employee = new Employee();
+        //employee.setId(1L);
+        employee.setFirstName("Jan");
+        employee.setLastName("Nowak");
+        employee.setSalary(3333.3);
+
+        Employee employee2 = new Employee();
+        //employee2.setId(1L);
+        employee2.setFirstName("Robert");
+        employee2.setLastName("Bedmarek");
+        employee2.setSalary(3333.3);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.persist(employee2);
+        entityManager.getTransaction().commit();
+
+
         entityManager.close();
         entityManagerFactory.close();
     }
